c6927462b472ccb70ca2c778489622eeeb0d0244
Kamola Kamola <kamolaxaytboyeva0509@gmail.com>
2024-03-05 16:14:29 +0500

    initial

--- article/urls.py
+++ article/urls.py
@@ -3,7 +3,11 @@ from .views import (
     article_list_view,
     article_detail_view,
     article_created_view,
-    article_list_create_api_view
+    article_list_create_api_view,
+    my_article_list_view,
+    article_update_view,
+    article_delete_view,
+    article_rud_view
 )
 
 app_name = 'article'
@@ -12,5 +16,9 @@ urlpatterns = [
     path('list/', article_list_view, name='list'),
     path('detail/<int:pk>/', article_detail_view, name='detail'),
     path('create/', article_created_view, name='create'),
-    path('list-create/', article_list_create_api_view, name='list-create')
+    path('list-create/', article_list_create_api_view, name='list-create'),
+    path('my-list/', my_article_list_view, name='my-list'),
+    path('update/<int:pk>/', article_update_view, name='update'),
+    path('delete/<int:pk>/', article_delete_view, name='delete'),
+    path('rud/<int:pk>/', article_rud_view, name='rud')
 ]
--- article/views.py
+++ article/views.py
@@ -6,11 +6,15 @@ from .models import Article
 from .serializers import ArticleSerializer, ArticlePostSerializer
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwnerOrReadonly
 
 
 @api_view(['GET'])
 def article_list_view(request):
     qs = Article.objects.all()
+    q = request.GET.get('q')
+    if q:
+        qs = qs.filter(title__icontains=q)
     serializer = ArticleSerializer(qs, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -45,7 +49,10 @@ def article_created_view(request):
 @permission_classes([IsAuthenticated])
 def article_list_create_api_view(request):
     if request.method == "POST":
-        serializer = ArticlePostSerializer(data=request.data)
+        context = {
+            'user_id': request.user.id
+        }
+        serializer = ArticlePostSerializer(data=request.data, context=context)
         if serializer.is_valid():
             serializer.save()
             obj = get_object_or_404(Article, pk=serializer.data.get('id'))
@@ -61,3 +68,57 @@ def article_list_create_api_view(request):
     qs = Article.objects.all()
     serializer = ArticleSerializer(qs, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def my_article_list_view(request):
+    user = request.user
+    article = Article.objects.filter(author_id=user.id)
+    serializer = ArticleSerializer(article, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['PUT', 'PATCH'])
+@permission_classes([IsAuthenticated, IsOwnerOrReadonly])
+def article_update_view(request, pk):
+    obj = get_object_or_404(Article, id=pk)
+    data = request.data
+    partial = False
+    if request.method == 'PATCH':
+        partial = True
+    serializer = ArticleSerializer(data=data, instance=obj, partial=partial)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated, IsOwnerOrReadonly])
+def article_delete_view(request, pk):
+    obj = get_object_or_404(Article, id=pk)
+    obj.delete()
+    return Response({"success": True, "message": "Article deleted"}, status=status.HTTP_204_NO_CONTENT)
+
+
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+@permission_classes([IsAuthenticated, IsOwnerOrReadonly])
+def article_rud_view(request, pk):
+    obj = get_object_or_404(Article, id=pk)
+    if request.method == 'GET':
+        qs = Article.objects.all()
+        serializer = ArticleSerializer(qs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        obj = get_object_or_404(Article, id=pk)
+        obj.delete()
+        return Response({"success": True, "message": "Article deleted"}, status=status.HTTP_204_NO_CONTENT)
+    else:
+        data = request.data
+        partial = False
+        if request.method == 'PATCH':
+            partial = True
+        serializer = ArticleSerializer(data=data, instance=obj, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
